05ff88232637aa5d8b71f504ba4b025cc118af26
Simon Glautier <simon.glautier@uclouvain.be>
2018-12-19 09:51:01 +0100

    OD-39 modification du fichier de migration 0040

--- migrations/0040_populate_educationgroup_fields.py
+++ migrations/0040_populate_educationgroup_fields.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+import pudb
 from django.db import migrations
 
 
@@ -11,9 +12,9 @@ def populate_educationgroup_fields(apps, shema_editor):
     OfferProposition = apps.get_model('dissertation', 'offerproposition')
     FacultyAdviser = apps.get_model('dissertation', 'facultyadviser')
 
-    offer_propositions = OfferProposition.objects.filter(education_group__isnull=True).all()
-    faculty_advisers = FacultyAdviser.objects.filter(education_group__isnull=True).all()
-    dissertations = Dissertation.objects.filter(education_group_year_start__isnull=True).all()
+    offer_propositions = OfferProposition.objects.filter(education_group__isnull=True)
+    faculty_advisers = FacultyAdviser.objects.filter(education_group__isnull=True)
+    dissertations = Dissertation.objects.filter(education_group_year_start__isnull=True)
     offer_ids = set([op.offer_id for op in offer_propositions])
     offer_year_ids = set([dissert.offer_year_start_id for dissert in dissertations])
 
@@ -24,44 +25,46 @@ def populate_educationgroup_fields(apps, shema_editor):
         map_offer_id_with_educ_group_id = {}
         for offer in Offer.objects.filter(pk__in=offer_ids).prefetch_related('offeryear_set'):
             if offer.offeryear_set.count() < 1:
-                raise Exception('WARNING :: No OfferYear found for offer = {}'.format(offer.id))
+                print('WARNING :: No OfferYear found for offer = {}'.format(offer.id))
+                continue
             off_year = offer.offeryear_set.order_by('-academic_year__year').first()
             try:
                 educ_group_id = map_acronym_with_educ_group_id[off_year.acronym]
+                map_offer_id_with_educ_group_id[offer.id] = educ_group_id
             except KeyError as e:
                 print('WARNING :: acronym {} does not have matching education group id.'.format(off_year.acronym))
-
-            map_offer_id_with_educ_group_id[offer.id] = educ_group_id
-
         return map_offer_id_with_educ_group_id
 
     map_offer_with_matching_education_group = get_map_offer_id_with_educ_grp(offer_ids)
 
     def get_map_education_group_year_id_with_offer_year(offer_year_ids):
-        education_group_years = EducationGroupYear.objects.all().values('id', 'acronym')
-        map_acronym_with_educ_group_year_id = {rec['acronym']: rec['id'] for rec in education_group_years}
+        education_group_years = EducationGroupYear.objects.all().values('id', 'acronym', 'academic_year__year')
+        map_acronym_with_educ_group_year_id = {str(rec['academic_year__year']) + rec['acronym'] : rec['id'] for rec in education_group_years}
         map_offer_year_id_with_educ_group_year_id = {}
         for offer_year in OfferYear.objects.filter(pk__in=offer_year_ids):
             try:
-                educ_group_year_id = map_acronym_with_educ_group_year_id[offer_year.acronym]
+                educ_group_year_id = map_acronym_with_educ_group_year_id[str(offer_year.academic_year.year) + offer_year.acronym]
             except KeyError as e:
                 print('WARNING :: acronym {} does not have matching education group id.'.format(offer_year.acronym))
 
             map_offer_year_id_with_educ_group_year_id[offer_year.id] = educ_group_year_id
+        print(map_offer_year_id_with_educ_group_year_id)
         return map_offer_year_id_with_educ_group_year_id
 
     for off_prop in offer_propositions:
-        off_prop.education_group_id = map_offer_with_matching_education_group[off_prop.offer_id]
-        off_prop.save()
+        educ_group_id = map_offer_with_matching_education_group.get(off_prop.offer_id, None)
+        if educ_group_id:
+            off_prop.education_group_id = educ_group_id
+            off_prop.save()
 
     for fac_adv in faculty_advisers:
-        fac_adv.education_group_id = map_offer_with_matching_education_group[fac_adv.offer_id]
+        fac_adv.education_group_id = map_offer_with_matching_education_group.get(fac_adv.offer_id, None)
         fac_adv.save()
 
     map_offer_year_with_matching_education_group_year = get_map_education_group_year_id_with_offer_year(offer_year_ids)
 
     for dissert in dissertations:
-        dissert.education_group_year_id = map_offer_year_with_matching_education_group_year[dissert.offer_year_start_id]
+        dissert.education_group_year_start_id = map_offer_year_with_matching_education_group_year.get(dissert.offer_year_start_id, None)
         dissert.save()
 
 
