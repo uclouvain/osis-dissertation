9190c2a36e2117c8fdf13a1800ee02f48c8c771c
BenJ <benjamin.daubry@uclouvain.be>
2019-12-10 11:07:26 +0100

    Indentation

--- views/information.py
+++ views/information.py
@@ -316,12 +316,14 @@ def manager_informations_add_person(request):
         if form.is_valid():
             data = form.cleaned_data
             if data['email'] and data['last_name'] and data['first_name']:
-                person = mdl.person.Person(email=data['email'],
-                                           last_name=data['last_name'],
-                                           first_name=data['first_name'],
-                                           phone=data['phone'],
-                                           phone_mobile=data['phone_mobile'],
-                                           source=person_source_type.DISSERTATION)
+                person = Person(
+                    email=data['email'],
+                    last_name=data['last_name'],
+                    first_name=data['first_name'],
+                    phone=data['phone'],
+                    phone_mobile=data['phone_mobile'],
+                    source=person_source_type.DISSERTATION
+                )
                 person.save()
                 adv = adviser.add(person, 'PRF', False, False, False, '')
                 return redirect('manager_informations_detail', pk=adv.pk)
